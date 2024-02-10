0d0ecdd7ce4478002265c07173c269589a6e5ae2
lewismcewan <ljamm6@gmail.com>
2024-02-10 13:03:46 +1300

    Implemented Request Validation Behaviour

--- WebApplication.Core/Common/Behaviours/RequestValidationBehaviour.cs
+++ WebApplication.Core/Common/Behaviours/RequestValidationBehaviour.cs
@@ -1,20 +1,40 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
+using ValidationException = FluentValidation.ValidationException;
 
 namespace WebApplication.Core.Common.Behaviours
 {
     public class RequestValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
     {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public RequestValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+        
         /// <inheritdoc />
         public async Task<TResponse> Handle(
             TRequest request,
             CancellationToken cancellationToken,
             RequestHandlerDelegate<TResponse> next)
         {
-            // TODO: throw a validation exception if there are any validation errors
-            // NOTE: the validation exception should contain all failures
+            var context = new ValidationContext<TRequest>(request);
+            var failures = _validators
+                .Select(x => x.Validate(context))
+                .SelectMany(x => x.Errors)
+                .Where(x => x != null)
+                .ToList();
 
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+            
             return await next();
         }
     }
